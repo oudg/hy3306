c7de74d31d316acd8c58882448d5ddfa9ceb479e
oudg <137664221@qq.com>
2022-10-09 22:38:13 +0800

    commit

---
+++ src/components/library/LibraryIndex.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container>
+    <el-aside style="width: 200px;margin-top: 20px">
+      <switch></switch>
+<SideMenu></SideMenu>
+    </el-aside>
+    <el-main>
+    <Books class="books-area"></Books>
+
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+ import SideMenu from './SideMenu'
+import Books from './Books'
+ export default {
+    name: 'AppLibrary',
+    components: {Books, SideMenu}
+  }
+</script>
+<style scoped>
+  .books-area {
+    width: 990px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
+
